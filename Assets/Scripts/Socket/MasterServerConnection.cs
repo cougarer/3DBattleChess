62723be8f1efcdadb6ffdddb52f561669f969bad
Max LykoS <863427604@qq.com>
2017-11-09 20:49:45 +0800

    2017.11.9 服务器框架学习1

--- Assets/Scripts/Socket/MasterServerConnection.cs
+++ Assets/Scripts/Socket/MasterServerConnection.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class MasterServerConnection : Singletion<MasterServerConnection> {
 
-    
+
 	void Start ()
     {
 		
